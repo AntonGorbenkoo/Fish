4a5451123caf099b60f0ce0a770fe1c570a8b65a
Denis <AntonGorbenko7@gmail.com>
2022-08-19 14:09:14 +0300

    Функция  Cjoice

--- Model.js
+++ Model.js
@@ -10,6 +10,11 @@ class Model {
     return dirF;
   }
 
+  async getQuestion(answer) {
+    let question = await fs.readFile(`./topics/${answer}`, 'utf-8');
+    console.log(question);
+  } 
+
 }
 
 module.exports = Model;
