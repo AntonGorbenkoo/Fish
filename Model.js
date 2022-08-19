1e2a6c253c53edb504883f9d395e3040dec78f87
Andrii Domanskyi <andriidomanskyi@MacBook-Air-Andrii.local>
2022-08-19 14:00:02 +0300

    getQuestions() release

--- Model.js
+++ Model.js
@@ -1,12 +1,26 @@
 const fs = require('fs').promises;
+
 class Model {
+  
   constructor(name, score) {
     this.name = name;
     this.score = score;
   }
+
   async getThemes() {
     let dirF =  await fs.readdir('./topics');
     return dirF;
+  }
+
+  async getQuestions(choice) {
+    const theme = await fs.readFile(`${__dirname}/topics/${choice}`);
+    const result = [];
+    const questions = theme.split('\n');
+    questions.forEach ((el, line) => {
+      if (!(line % 5)) result.push(el);
+      return;
+    });
+    return result;
   }
 }
 module.exports = Model;
